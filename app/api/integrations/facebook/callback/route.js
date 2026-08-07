b52194fea8ea124fa0df59f314b767fdd1ed7c31
Dinoxonline <dino@leclubbbq.nl>
2026-08-07 19:45:34 +0200

    Store Facebook page read authorization

--- app/api/integrations/facebook/callback/route.js
+++ app/api/integrations/facebook/callback/route.js
@@ -5,6 +5,7 @@ import { encryptMetaToken, getFacebookConfiguration, readMetaState } from "../..
 const GRAPH_VERSION = "v25.0";
 const GRANTED_SCOPES = [
   "pages_show_list",
+  "pages_read_engagement",
   "business_management",
 ];
 
