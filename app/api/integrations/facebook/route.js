f90726974363a14db696f780e44e3b6bf74dcc77
Dinoxonline <dino@leclubbbq.nl>
2026-08-07 19:34:53 +0200

    Keep Facebook connection on approved scopes

--- app/api/integrations/facebook/route.js
+++ app/api/integrations/facebook/route.js
@@ -4,11 +4,6 @@ import { createMetaState, getFacebookConfiguration } from "../../../../lib/meta-
 
 const SCOPES = [
   "pages_show_list",
-  "pages_read_engagement",
-  "pages_read_user_content",
-  "pages_manage_engagement",
-  "ads_read",
-  "read_insights",
   "business_management",
 ];
 
