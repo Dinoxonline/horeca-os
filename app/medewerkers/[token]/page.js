b9880521d5e094586d919b40a3042c011143206c
Dinoxonline <dino@leclubbbq.nl>
2026-09-05 13:58:26 +0200

    Render public staff ticket form

--- app/medewerkers/[token]/page.js
+++ app/medewerkers/[token]/page.js
@@ -1,2 +1,7 @@
-export default function StaffTicketPage() { return null; }
+import StaffTicketForm from "../../../components/staff-ticket-form";
+
+export default async function StaffTicketPage({ params }) {
+  const { token } = await params;
+  return <StaffTicketForm token={token} />;
+}
 
